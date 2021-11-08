beb3609880669a9db8b3c3f1abb64034cb60978d
pawel.kania <pawel.kania@accenture.com>
2021-11-08 14:45:47 +0100

    initial-project-structure

---
+++ email-service/src/test/java/io/github/pavello/rabbitmq/sample/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pavello.rabbitmq.sample.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
